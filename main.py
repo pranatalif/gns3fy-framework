8c1d23bccbdb13a3f2a089f411cf6b93d63c6d64
pranatalif <alifakbarp@gmail.com>
2020-03-07 00:26:32 +0100

    create nodes and links

--- main.py
+++ main.py
@@ -1,59 +1,86 @@
 from gns3fy import Gns3Connector, Project, Node, Link
 import json
-import configparser
-import config
+from json import dumps
+import yaml
+import time
 
-SERVER_URL = "http://10.0.0.4:3080"
-server = Gns3Connector(url=SERVER_URL)
+global CONFIG
+global server
+global projectID
 
-def getVersion():
+def getVersion(server):
     print(server.get_version())
+    #exit(1)
 
-def listProject():
-    server = Gns3Connector(url=SERVER_URL)
+def listProject(server):
     print(server.projects_summary())
-    #server.delete_project(lab.project_id)
 
-def createProject(projectName):
-    lab = Project(name=projectName, connector=server)
-    getProject = next((project for project in server.projects_summary(is_print=False) if project[0] == lab.name), "Project not found")
-    if getProject[0] == lab.name:
-        print("Project name existed, re-creating the project...")
+def createProject(server, project):
+    projectObj = Project(name=project, connector=server)
+    getProject = next((project for project in server.projects_summary(is_print=False) if project[0] == projectObj.name), "Project not found")
+    if getProject[0] == projectObj.name:
+        print("Project name existed, re-creating the project...", end="")
         deleteProject(getProject[1])
-    else:
-        print("Creating the project...")
-    lab.create()
-    projectID = lab.project_id
-    print("Project " + lab.name + " created. ID: " + lab.project_id)
+    #else:
+    #    print("Creating the project...", end="")
+    projectObj.create()
+    projectID = projectObj.project_id
+    print("Project " + projectObj.name + " created. ID: " + projectObj.project_id + "...", end="")
     return projectID
 
 def deleteProject(id):
+    server = Gns3Connector(url="http://"+CONFIG["gns3_server"]+":"+CONFIG["gns3_port"])
     server.delete_project(id)
 
-def createNode(projectID):
-    print(server.get_templates())
-    for template in server.get_templates():
-        if "dock" in template["name"]:
-            print(f"Template: {template['name']} -- ID: {template['template_id']}")
+def createNodes(server, projectID):
+    nodeDict = dict()
+    for nodes in CONFIG["nodes"]:
+        node = Node(project_id=projectID, connector=server, template=nodes["appliance_name"], name=nodes["node_name"], x=nodes["x"], y=nodes["y"])
+        node.create()
+        nodeDict.update({node.name : node.node_id})
+    
+    return nodeDict
 
-    switch = Node(project_id=projectID, connector=server, name='EthSwitch', template='Ethernet switch')
-    #switch.create()
+def createLinks(server, projectID, nodeDict):  
+    for links in CONFIG["links"]:
+        if all (key in nodeDict for key in (links["node1"], links["node2"])):
+            node1ID=nodeDict[links["node1"]]
+            node2ID=nodeDict[links["node2"]]
+        else:
+            print("Neither node1 or node2 object found. Check the spelling of the nodes name")
+            break
 
-if __name__ == '__main__':
-    config = configparser.RawConfigParser()
-    config.read('config.properties')
-    projectName = config.get('Project', 'name')
+        nodeCombination = [dict(node_id=node1ID, adapter_number=0, port_number=links["port_num1"]), dict(node_id=node2ID, adapter_number=0, port_number=links["port_num2"])]
+        link = Link(project_id=projectID, connector=server, nodes=nodeCombination)
+        link.create()
 
-    ### Check connectivity and get the server version
-    getVersion()
 
+if __name__ == "__main__":
+    ### Read configuration file
+    with open("config.yml") as config_file:
+        CONFIG = yaml.full_load(config_file)
+      
+    ### Set connectivity and get the server version
+    server = Gns3Connector(url="http://"+CONFIG["gns3_server"]+":"+CONFIG["gns3_port"]) 
+    getVersion(server)
+    
     ### List all created projects
-    listProject()
+    #listProject(server)
 
     ### Create project and get its ID
-    print("Creating GNS3 project")
-    projectID = createProject(projectName)
+    print("Creating GNS3 project...", end="")
+    projectID = createProject(server, CONFIG["project_name"])
+    print("OK")
+    time.sleep(1)
 
-    ### Create node
-    print("Creating node")
-    createNode(projectID)
+    ### Create nodes
+    print("Creating nodes...", end="")
+    nodeDict = createNodes(server, projectID)
+    print("OK")
+    time.sleep(1)
+
+    ### Create links
+    print("Creating links...", end="")
+    createLinks(server, projectID, nodeDict)
+    print("OK")
+    time.sleep(1)
